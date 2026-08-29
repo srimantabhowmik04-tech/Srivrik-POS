541d825d630a97486c9e3057645fab23432f22fb
srimantabhowmik04-tech <srimantabhowmik04@gmail.com>
2026-08-29 12:20:06 +0530

    Add color definitions for UI theme

---
+++ ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.atoz.pos.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val RoyalBlue = Color(0xFF0D47A1)
+val CrimsonRed = Color(0xFFD32F2F)
+val PureWhite = Color(0xFFFFFFFF)
+val OffWhite = Color(0xFFF4F6F9)
+val DarkText = Color(0xFF212121)
+val SlateGray = Color(0xFF757575)
+val LightBorder = Color(0xFFE0E0E0)
+val SuccessGreen = Color(0xFF2E7D32)
